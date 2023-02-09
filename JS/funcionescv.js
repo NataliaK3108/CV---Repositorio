c61d6ed34976033cca835a85501f67c06124bc8f
Natalia Karadagian <nkaradagian@gmail.com>
2023-02-09 19:29:07 -0300

    intento

---
+++ JS/funcionescv.js
@@ -0,0 +1,5 @@
+
+document.getElementById("boton").onclick = function () {
+    console.log("Sus datos fueron enviados");
+    document.getElementById("cv").innerHTML = "Gracias por contactarse, me comunicaré a la brevedad."
+}
